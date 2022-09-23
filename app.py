b326c069436f2968689f64276541609bccf7a320
hu1139744939 <hu1139744939@163.com>
2022-09-23 14:14:53 +0800

    --alert

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
+from email import message
 import os
+from pyexpat.errors import messages
 import sys
 
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask, render_template, request, url_for, redirect, flash
 from flask_login import UserMixin, LoginManager, login_user, login_required, logout_user, current_user
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'dev')
@@ -11,8 +14,9 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////' + os.path.join(app.root_pa
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
-
-
+UPLOAD_FOLDER = './static/images_temp'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 class User(db.Model, UserMixin):  # 表名将会是 user（自动生成，小写处理）
     id = db.Column(db.Integer, primary_key=True)
@@ -35,9 +39,29 @@ def initdb(drop):
     db.create_all()
     click.echo('Initialized database.')  # 输出提示信息
 
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    msg=""
+    if request.method == 'POST':
+        # if 'file' not in request.files:
+        #     msg="No file part"
+        #     return render_template('index.html', messages=msg)
+        
+        f = request.files['file']    
+        if 'file' not in request.files or f.filename == '':
+            msg="No selected file"
+
+        if f:
+            # filename = secure_filename(f.filename)
+            upload_path = os.path.join(os.path.dirname(__file__), 'static','upload_file_dir', f.filename)
+            upload_path = os.path.abspath(upload_path)
+            f.save(upload_path)
+            msg="successfully"
+
+    return render_template('index.html', messages=msg)
 
 @app.route('/about', methods=['GET', 'POST'])
 def about():
@@ -63,25 +87,22 @@ def load_user(user_id):
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    msg=""
     if request.method == 'POST':
         username_html = request.form['name']
         password_html = request.form['password']
 
         if not username_html or not password_html:
-            flash('Invalid input.')
-            return redirect(url_for('login'))
+            msg="none input"
 
         user = User.query.filter(User.username == username_html).first()
-
         if username_html == user.username and password_html == user.password:
             login_user(user)
-            flash('Login success.')
-            return redirect(url_for('index'))
+            msg="Login success"
+        else:
+            msg="Invalid username or password"
 
-        flash('Invalid username or password.')
-        return redirect(url_for('login'))
-
-    return render_template('login.html')
+    return render_template('login.html',messages=msg)
 
 @app.route('/logout')
 @login_required
@@ -92,6 +113,30 @@ def logout():
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+    if request.method == 'POST':
+        username_html = request.form['email']
+        password_html = request.form['password']
+        if username_html and password_html:
+            user = User.query.filter(User.username == username_html).first()
+            if username_html == user.username and password_html == user.password:
+                login_user(user)
+                flash('Login success.')
+                return redirect(url_for('index'))    
+            else:
+                flash('Error username or password.')
+        else:
+            flash('Invalid input.')
+            return redirect(url_for('register'))
+
+        register_name_html = request.form['name']
+        register_password_html = request.form['password']
+        if register_name_html and register_password_html:
+            me = User(register_name_html, register_password_html)
+            db.session.add(me)
+            db.session.commit()
+        
+        return redirect(url_for('register'))
+
     return render_template('register.html')
 
 login_manager.login_view = 'login'
